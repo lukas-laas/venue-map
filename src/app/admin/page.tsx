1e2eb88911c77e0a89b152a6f8a3e75e9ac0eeb9
lukas-laas <lukas.s.laas@gmail.com>
2024-08-15 09:24:45 +0200

    display suggestions and venues in admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -6,16 +6,49 @@ export default async function Page() {
   const suggestions = await getAllSuggestions();
   return (
     <>
-      <h1>Admin page!</h1>
-      <AdminForm />
-      <h2>Venues</h2>
-      {venues?.map((venue) => {
-        return <p key={venue.id}>{venue.name}</p>;
-      })}
-      <h2>Suggestions</h2>
-      {suggestions?.map((suggestion) => {
-        return <p key={suggestion.id}>{suggestion.name}</p>;
-      })}
+      <main className="p-4">
+        <h1 className="text-2xl">Admin page!</h1>
+        <section className="flex w-full max-w-4xl justify-between gap-4">
+          <div className="max-w-xl w-full min-w-80">
+            <h2>Add Venue</h2>
+            <AdminForm />
+          </div>
+          <div>
+            <h2>Suggestions</h2>
+            <ul>
+              {suggestions?.map((suggestion) => {
+                return (
+                  <li
+                    key={suggestion.id}
+                    className="p-2 gap-1 flex flex-col border-black border w-full min-w-72 rounded-sm"
+                  >
+                    <h3 className="text-lg">{suggestion.name}</h3>
+                    <p>{suggestion.address}</p>
+                    <p>{suggestion.description}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div>
+            <h2>Venues</h2>
+            <ul>
+              {venues?.map((venue) => {
+                return (
+                  <li
+                    key={venue.id}
+                    className="p-2 gap-1 flex flex-col border-black border w-full  min-w-72 rounded-sm"
+                  >
+                    <h3 className="text-lg">{venue.name}</h3>
+                    <p>{venue.address}</p>
+                    <p>{venue.description}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
